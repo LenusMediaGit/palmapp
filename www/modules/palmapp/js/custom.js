2442f8e0061178c09babd1dd7f81be7c2342c92a
Yehonal <g.ronca@hyperweb2.com>
2016-05-16 12:42:27 +0200

    updated

--- www/modules/palmapp/js/custom.js
+++ www/modules/palmapp/js/custom.js
@@ -6,8 +6,8 @@ var origUrl = "http://palmapp.lenuslab.com/web/?version=1&platform=phonegap&phon
 
 if (!appFramework.getConf("url"))
     appFramework.setConf("url", origUrl);
-    
-   
+
+
 // Depending on the device, a few examples are:
 //   - "Android"
 //   - "BlackBerry 10"
@@ -17,7 +17,7 @@ if (!appFramework.getConf("url"))
 var devicePlatform = device.platform;
 
 
-if (devicePlatform === "Android" && PushNotification) {
+if (PushNotification) {
 	var push = PushNotification.init({
 		android: {
 			senderID: "993634988218"
@@ -27,7 +27,7 @@ if (devicePlatform === "Android" && PushNotification) {
 			badge: "true",
 			sound: "true"
 		},
-		windows: {} 
+		windows: {}
 	});
 
 	push.on('registration', function (data) {
@@ -73,11 +73,11 @@ if (devicePlatform === "Android" && PushNotification) {
 
 // run external app
 appFramework.loadExternal({
-	
+
     onReady: function () {
         // restore original url ( can be changed by notifications )
         appFramework.setConf("url", origUrl);
-		
+
         /*jQuery("#wrapper").css("background-image"," url('/android_asset/www/data/img/ajax-loader.gif')");
         jQuery("#wrapper").css("background-repeat","no-repeat");
         jQuery("#wrapper").css("background-attachment","fixed");
@@ -85,7 +85,7 @@ appFramework.loadExternal({
         jQuery("#wrapper").css("background-position","center");*/
 
         /*var iframe = jQuery("#iframe-wrapper");
-         
+
          // just 2 tricks to make fading working on android:
          // 1) use css instead of jquery fade
          // 2) set a small timeout after iframe ready before "launche" the animation ( workaround )
